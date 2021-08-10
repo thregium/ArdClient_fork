a56160d4a8c9bb95426673ba96374c184fab5c1e
Cediner <cedivargardner@gmail.com>
2021-08-10 19:33:09 +0300

    -- another taketrays fix +Update

--- src/haven/automation/TakeTrays.java
+++ src/haven/automation/TakeTrays.java
@@ -1,13 +1,9 @@
 package haven.automation;
 
 
-import haven.Coord;
-import haven.GItem;
 import haven.GameUI;
-import haven.Inventory;
-import haven.WItem;
-import haven.Widget;
 import haven.purus.pbot.PBotInventory;
+import haven.purus.pbot.PBotItem;
 import haven.purus.pbot.PBotUtils;
 
 import java.util.List;
@@ -23,17 +19,15 @@ public class TakeTrays implements Runnable {
     @Override
     public void run() {
         new OpenRacks(gui).run();
-        List<WItem> trays;
 
         for (PBotInventory q : PBotUtils.getAllInventories(gui.ui)) {
             try {
                 if (q.inv != gui.maininv) {
-                    trays = (q.inv.getItemsPartial("Tray"));
+                    List<PBotItem> trays = q.getInventoryItemsByNames("Tray");
                     System.out.println("trays2 size : " + trays.size());
                     if (!trays.isEmpty()) {
-                        for (WItem item : trays)
-                            item.item.wdgmsg("transfer", new Coord(item.item.sz.x / 2, item.item.sz.y / 2), -1);
-                        trays.clear();
+                        for (PBotItem item : trays)
+                            item.transferItem();
                     }
                 }
             } catch (Exception e) {
